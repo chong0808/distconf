4b0c4aa62446ad04843cf80a7ef439c452bbb04c
zhengchong <730427512@qq.com>
2019-07-14 10:53:22 +0800

    feat: new Project

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,38 @@
 const fs = require('fs')
 
-exports.exists = function (_path) {
-  if (!(fs.existsSync(_path) && fs.statSync(_path).isDirectory(_path))) {
-    fs.mkdirSync(_path)
+exports.exists =  (_path) => fs.existsSync(_path) && fs.statSync(_path).isDirectory(_path)
+
+exports.promisify = (git) => {
+  git.pullAsync = async function () {
+    return new Promise((resolve, reject) => {
+      git.pull((err, data) => {
+        if (err) reject(err);
+        else resolve(data)
+      })
+    })
+  }
+  git.checkoutAsync = async function (branchname) {
+    return new Promise((resolve, reject) => {
+      git.checkout(branchname, function (err, data) {
+        if (err) reject(err);
+        else resolve(data)
+      })
+    })
+  }
+  git.pullAsync = async function () {
+    return new Promise((resolve, reject) => {
+      git.pull((err, data) => {
+        if (err) reject(err);
+        else resolve(data)
+      })
+    })
+  }
+  git.cloneAsync = async function (repoUrl) {
+    await new Promise((resolve, reject) => {
+      git.clone(repoUrl, (err, data) => {
+        if (err) reject(err);
+        else resolve(data)
+      })
+    })
   }
 }
